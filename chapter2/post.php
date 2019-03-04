3efeeb02feb87d768940816819287d0550b377c8
ryo1and1 <nyaho8056@gmail.com>
2019-03-04 18:27:48 +0900

    post R

--- chapter2/post.php
+++ chapter2/post.php
@@ -6,73 +6,40 @@ header("Location:login.php");
 exit;
 }
 
-$yourname = $_POST['yourname'];
-$text = $_POST['text'];
-$dataFile = 'bbs.dat';
-
-function h($s){
-    return htmlspecialchars($s, ENT_QUOTES, 'utf-8')
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' &&
-    isset($yourname) &&
-    isset($text)){
-    
-
-        if ($text !== ''){
-            $yourname = ($yourname === '') ? 'ななしさん' : $yourname;
-            
-            $yourname = str_replace("\t", ' ', $yourname);
-            $text = str_replace("\t", ' ', $text);
-
-            $postedAt = date('Y-m-d H:i:s');
-
-            $newData = $yourname . "\t" . $text . "\t" .$postedAt . "\n";
-
-            $fp = fopen($dataFile, 'a');
-            fwrite($fp,$newData);
-            fclose($fp);
-        }
-}
-
-$posts = file($dataFile, FILE_IGNORE_NEW_LINES);
-$posts = array_reverse($posts);
-
+  function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES);
+  }
+  $rows = json_decode(file_get_contents('bbs.json'), true);
+  if (!empty($_POST['write'])) {
+    $row = array(
+      'name' => $_POST['name'],
+      'title' => $_POST['title'],
+      'contents' => $_POST['contents'],
+      'time' => date("Y/m/d H:i:s")
+    );
+    array_unshift($rows, $row);
+    file_put_contents('bbs.json', json_encode($rows));
+  }
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-<meta charset="UTF-8">
-<title>投稿ページ</title>
+<meta charset="utf-8">
+<title>掲示板</title>
 </head>
 <body>
-<h1>掲示板</h1>
-<section>
-    <h1>新規投稿</h1>
-    <form action="" method="post">
-        <label>名前</label><br>
-        <input type="text" name="yourname" value="<?php echo $yourname; ?>"><br>
-        <label>本文</label><br> 
-        <input type="text" name="text" value=""><br>
-        <button type="submit">投稿</button>
-    </form>
-</section>
-<section>
-    <h2>投稿一覧 (<?php echo count($posts); ?>件)</h2>
-    <ul>
-    <?php if (count($posts)) : ?>
-    <?php foreach ($posts as $post) : ?>
-    <?php lists($yourname, $postedAt) = explode("\t", $post); ?>
-        <li><?php echo h($yourname); ?><br>
-        <?php echo h($text); ?><br>
-        <?php echo h($postedAt); ?> 
-        </li>
-    <?php endforeach; ?>
-    <?php else : ?>
-        <p>投稿はまだありません</p>
-    <?php endif; ?>
-    </ul>
-</section>
+<form method="post">
+お名前: <input name="name"><br>
+題名: <input name="title"><br>
+<textarea name="contents" cols="60" rows="5"></textarea><br>
+<input type="submit" name="write" value="送信">
+</form>
+<hr>
+<?php foreach($rows as $row): ?>
+  <strong><?php echo h($row['title']) ?></strong>
+  <br><small>投稿者：<?php echo h($row['name']) . ' ' . h($row['time']) ?></small>
+  <p><?php echo nl2br(h($row['contents']), false) ?></p>
+  <hr>
+<?php endforeach ?>
 </body>
 </html>
